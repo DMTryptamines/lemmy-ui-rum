1d3f59642ccdece9703a1bdfe0f100f1b8d7c08c
DMTryptamines <DMTryptamines@protonmail.com>
2022-05-31 08:33:43 -0300

    adding elastic-rum dependencies

--- src/server/rum.js
+++ src/server/rum.js
@@ -1,17 +1,16 @@
-import { init as initApm } from '@elastic/apm-rum'
+import { init as initApm } from "@elastic/apm-rum";
 var apm = initApm({
-
   // Set required service name (allowed characters: a-z, A-Z, 0-9, -, _, and space)
-  serviceName: 'lemmy',
+  serviceName: "lemmy",
 
   // Set custom APM Server URL (default: http://localhost:8200)
-  serverUrl: 'https://10.162.0.4:8200',
+  serverUrl: "https://localhost:8200",
 
   // Set the service version (required for source map feature)
-  serviceVersion: '0.16.3',
+  serviceVersion: "0.16.5",
 
   // Set the service environment
-  environment: 'production'
-  // distributedTracingOrigins: ['http://localhost:8080'], 
-})
+  environment: "production",
+  // distributedTracingOrigins: ['http://localhost:8080'],
+});
 export default apm;
